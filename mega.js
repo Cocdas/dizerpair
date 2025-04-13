9cd3a28ff13421c5e7bbfc613cbf33ca7e0f3ce1
Cocdas <138351304+Cocdas@users.noreply.github.com>
2025-04-13 05:20:06 -0700

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'naqajoxy@logsmarter.net',
-    password: 'pair@2024',
+    email: 'mogirove@cyclelove.cc',
+    password: 'NPF2qEU6_Li_k-F',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
